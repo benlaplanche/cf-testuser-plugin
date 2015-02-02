501d81bc9c21ee63f28f38ddc0e9e4568ec46b00
Ben Laplanche <blaplanche@pivotal.io>
2015-02-02 18:09:56 +0000

    initial commit

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/cloudfoundry/cli/plugin"
+)
+
+type TestUser struct{}
+
+func (c *TestUser) Run(cliConnection plugin.CliConnection, args []string) {
+	fmt.Println("running the new test user command")
+	fmt.Println(args)
+}
+
+func (c *TestUser) GetMetadata() plugin.PluginMetadata {
+	return plugin.PluginMetadata{
+		Name: "TestUser",
+		Version: plugin.VersionType{
+			Major: 0,
+			Minor: 1,
+			Build: 0,
+		},
+		Commands: []plugin.Command{
+			{
+				Name:     "test-user",
+				HelpText: "Command to create a test user and development org & space and grant all permissions",
+				UsageDetails: plugin.Usage{
+					Usage: "test-user username password",
+				},
+			},
+		},
+	}
+}
+
+func main() {
+	plugin.Start(new(TestUser))
+}
---
+++ main_test.go
@@ -0,0 +1,30 @@
+package main_test
+
+import (
+	"github.com/cloudfoundry/cli/plugin/fakes"
+	. "github.com/cloudfoundry/cli/plugin_examples/call_cli_cmd/main"
+	io_helpers "github.com/cloudfoundry/cli/testhelpers/io"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("TestUserCmd", func() {
+	Describe(".Run", func() {
+		var fakeCliConnection *fakes.FakeCliConnection
+		var callCliCommandPlugin *TestUser
+
+		BeforeEach(func() {
+			fakeCliConnection = &fakes.FakeCliConnection{}
+			callCliCommandPlugin = &CliCmd{}
+		})
+
+		It("returns the correct output", func() {
+			io_helpers.CaptureOutput(func() {
+				callCliCommandPlugin.run(fakeCliConnection, []string{"test-user"})
+			})
+
+			Expect(fakeCliConnection.CliCommandArgsForCall(0)[0]).To(Equal("running the new test user command"))
+			Expect(fakeCliConnection.CliCommandArgsForCall(0)[0]).To(Equal("test-user"))
+		})
+	})
+})
