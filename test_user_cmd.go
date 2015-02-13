b1831001be58bbafbd260826cc25f25dd65eeebe
Ben Laplanche <blaplanche@pivotal.io>
2015-02-13 18:11:49 +0000

    refactor in progress to loop over functions instead

--- test_user_cmd.go
+++ test_user_cmd.go
@@ -8,6 +8,19 @@ import (
 
 type TestUser struct{}
 
+var commands = []string{
+	c.CreateUser(cliConnection, args),
+	c.CreateOrg(cliConnection, args),
+	c.CreateSpace(cliConnection, args),
+	c.AssignOrgRole(cliConnection, args, "OrgManager", "4"),
+	c.AssignOrgRole(cliConnection, args, "BillingManager", "5"),
+	c.AssignOrgRole(cliConnection, args, "OrgAuditor", "6"),
+	c.AssignSpaceRole(cliConnection, args, "SpaceManager", "7"),
+	c.AssignSpaceRole(cliConnection, args, "SpaceDeveloper", "8"),
+	c.AssignSpaceRole(cliConnection, args, "SpaceAuditor", "9"),
+	c.SwitchUser(cliConnection, args)
+}
+
 func (c *TestUser) GetMetadata() plugin.PluginMetadata {
 	return plugin.PluginMetadata{
 		Name: "TestUser",
@@ -38,85 +51,78 @@ func (c *TestUser) Run(cliConnection plugin.CliConnection, args []string) {
 		fmt.Println("Incorrect usage")
 		fmt.Println(c.GetMetadata().Commands[0].UsageDetails.Usage)
 	} else {
-		c.CreateUser(cliConnection, args)
-		c.CreateOrg(cliConnection, args)
-		c.CreateSpace(cliConnection, args)
-
-		c.AssignOrgRole(cliConnection, args, "OrgManager", "4")
-		c.AssignOrgRole(cliConnection, args, "BillingManager", "5")
-		c.AssignOrgRole(cliConnection, args, "OrgAuditor", "6")
-
-		c.AssignSpaceRole(cliConnection, args, "SpaceManager", "7")
-		c.AssignSpaceRole(cliConnection, args, "SpaceDeveloper", "8")
-		c.AssignSpaceRole(cliConnection, args, "SpaceAuditor", "9")
-
-		c.SwitchUser(cliConnection, args)
+		for _, v := range commands {
+			if err := v(); err != nil {
+				break
+			}
+		}
 	}
 }
 
 func (c *TestUser) CreateUser(cliConnection plugin.CliConnection, args []string) {
 
-	output, err := cliConnection.CliCommandWithoutTerminalOutput("create-user", args[1], args[2])
+	_, err := cliConnection.CliCommandWithoutTerminalOutput("create-user", args[1], args[2])
 
 	if err != nil {
 		fmt.Println(colorstring.Color("[red][1/10]  Created user " + args[1]))
-		fmt.Println(err)
+		// fmt.Println(err)
+		// os.Exit(1)
 	} else {
 		fmt.Println(colorstring.Color("[green][1/10]  Created user " + args[1]))
-		fmt.Println(output)
+		// fmt.Println(output)
 	}
 
 }
 
 func (c *TestUser) CreateOrg(cliConnection plugin.CliConnection, args []string) {
 
-	output, err := cliConnection.CliCommandWithoutTerminalOutput("create-org", "development")
+	_, err := cliConnection.CliCommandWithoutTerminalOutput("create-org", "development")
 
 	if err != nil {
 		fmt.Println(colorstring.Color("[red][2/10]  Created Organisation development"))
-		fmt.Println(err)
+		// fmt.Println(err)
 	} else {
 		fmt.Println(colorstring.Color("[green][2/10]  Created Organisation development"))
-		fmt.Println(output)
+		// fmt.Println(output)
 	}
 }
 
 func (c *TestUser) CreateSpace(cliConnection plugin.CliConnection, args []string) {
 
-	output, err := cliConnection.CliCommandWithoutTerminalOutput("create-space", "development", "-o", "development")
+	_, err := cliConnection.CliCommandWithoutTerminalOutput("create-space", "development", "-o", "development")
 
 	if err != nil {
 		fmt.Println(colorstring.Color("[red][3/10]  Created Space development"))
-		fmt.Println(err)
+		// fmt.Println(err)
 	} else {
 		fmt.Println(colorstring.Color("[green][3/10]  Created Space development"))
-		fmt.Println(output)
+		// fmt.Println(output)
 	}
 }
 
 func (c *TestUser) AssignOrgRole(cliConnection plugin.CliConnection, args []string, role string, i string) {
 
-	output, err := cliConnection.CliCommand("set-org-role", args[1], "development", role)
+	_, err := cliConnection.CliCommand("set-org-role", args[1], "development", role)
 
 	if err != nil {
 		fmt.Println(colorstring.Color("[red][" + i + "/10]  Assigned " + role + " to me in Org development"))
-		fmt.Println(err)
+		// fmt.Println(err)
 	} else {
 		fmt.Println(colorstring.Color("[green][" + i + "/10]  Assigned " + role + " to me in Org development"))
-		fmt.Println(output)
+		// fmt.Println(output)
 	}
 }
 
 func (c *TestUser) AssignSpaceRole(cliConnection plugin.CliConnection, args []string, role string, i string) {
 
-	output, err := cliConnection.CliCommand("set-space-role", args[1], "development", "development", role)
+	_, err := cliConnection.CliCommand("set-space-role", args[1], "development", "development", role)
 
 	if err != nil {
 		fmt.Println(colorstring.Color("[red][" + i + "/10]  Assigned " + role + " to me in Space development"))
-		fmt.Println(err)
+		// fmt.Println(err)
 	} else {
 		fmt.Println(colorstring.Color("[green][" + i + "/10]  Assigned " + role + " to me in Space development"))
-		fmt.Println(output)
+		// fmt.Println(output)
 	}
 }
 
--- test_user_cmd_test.go
+++ test_user_cmd_test.go
@@ -19,51 +19,72 @@ var _ = Describe("TestUserCmd", func() {
 			fakeCliConnection = &fakes.FakeCliConnection{}
 			callCliCommandPlugin = &TestUser{}
 
-			fakeCliConnection.CliCommandWithoutTerminalOutputStub =
-				func(args ...string) ([]string, error) {
-					if args[0] == "create-user" {
-						return nil, errors.New("create user failed")
+		})
+
+		Describe("Testing the CLI arguments", func() {
+			It("returns an error if incorrect number of args supplied", func() {
+
+				output := io_helpers.CaptureOutput(func() {
+					callCliCommandPlugin.Run(fakeCliConnection, []string{"test-user"})
+				})
+
+				Expect(output[0]).To(Equal("Incorrect usage"))
+				Expect(output[1]).To(Equal("cf test-user <username> <password>"))
+			})
+		})
+
+		Describe("Testing the happy path", func() {
+
+			It("creates a new user", func() {
+				output := io_helpers.CaptureOutput(func() {
+					callCliCommandPlugin.Run(fakeCliConnection, []string{"test-user", "me", "password"})
+				})
+
+				Expect(output[0]).To(Equal(colorstring.Color("[green][1/10]  Created user me")))
+
+				Expect(output[1]).To(Equal(colorstring.Color("[green][2/10]  Created Organisation development")))
+
+				Expect(output[2]).To(Equal(colorstring.Color("[green][3/10]  Created Space development")))
+
+				Expect(output[3]).To(Equal(colorstring.Color("[green][4/10]  Assigned OrgManager to me in Org development")))
+
+				Expect(output[4]).To(Equal(colorstring.Color("[green][5/10]  Assigned BillingManager to me in Org development")))
+
+				Expect(output[5]).To(Equal(colorstring.Color("[green][6/10]  Assigned OrgAuditor to me in Org development")))
+
+				Expect(output[6]).To(Equal(colorstring.Color("[green][7/10]  Assigned SpaceManager to me in Space development")))
+
+				Expect(output[7]).To(Equal(colorstring.Color("[green][8/10]  Assigned SpaceDeveloper to me in Space development")))
+
+				Expect(output[8]).To(Equal(colorstring.Color("[green][9/10]  Assigned SpaceAuditor to me in Space development")))
+
+				Expect(output[9]).To(Equal(colorstring.Color("[green][10/10]  Logged out and logged in as me")))
+
+			})
+
+		})
+
+		Describe("Cannot create a user", func() {
+
+			BeforeEach(func() {
+				fakeCliConnection.CliCommandWithoutTerminalOutputStub =
+					func(args ...string) ([]string, error) {
+						if args[0] == "create-user" {
+							return nil, errors.New("create user failed")
+						}
+						return nil, nil
 					}
-					return nil, nil
-				}
-
-		})
-
-		It("returns an error if incorrect number of args supplied", func() {
-
-			output := io_helpers.CaptureOutput(func() {
-				callCliCommandPlugin.Run(fakeCliConnection, []string{"test-user"})
 			})
 
-			Expect(output[0]).To(Equal("Incorrect usage"))
-			Expect(output[1]).To(Equal("cf test-user <username> <password>"))
-		})
+			It("returns an error", func() {
+				output := io_helpers.CaptureOutput(func() {
+					callCliCommandPlugin.Run(fakeCliConnection, []string{"test-user", "me", "password"})
+				})
 
-		It("creates a new user", func() {
-			output := io_helpers.CaptureOutput(func() {
-				callCliCommandPlugin.Run(fakeCliConnection, []string{"test-user", "me", "password"})
+				Expect(output[0]).To(Equal(colorstring.Color("[red][1/10]  Created user me")))
+
+				Expect(len(output)).To(Equal(1))
 			})
-
-			Expect(output[0]).To(Equal(colorstring.Color("[green][1/10]  Created user me")))
-
-			Expect(output[2]).To(Equal(colorstring.Color("[green][2/10]  Created Organisation development")))
-
-			Expect(output[4]).To(Equal(colorstring.Color("[green][3/10]  Created Space development")))
-
-			Expect(output[6]).To(Equal(colorstring.Color("[green][4/10]  Assigned OrgManager to me in Org development")))
-
-			Expect(output[8]).To(Equal(colorstring.Color("[green][5/10]  Assigned BillingManager to me in Org development")))
-
-			Expect(output[10]).To(Equal(colorstring.Color("[green][6/10]  Assigned OrgAuditor to me in Org development")))
-
-			Expect(output[12]).To(Equal(colorstring.Color("[green][7/10]  Assigned SpaceManager to me in Space development")))
-
-			Expect(output[14]).To(Equal(colorstring.Color("[green][8/10]  Assigned SpaceDeveloper to me in Space development")))
-
-			Expect(output[16]).To(Equal(colorstring.Color("[green][9/10]  Assigned SpaceAuditor to me in Space development")))
-
-			Expect(output[18]).To(Equal(colorstring.Color("[green][10/10]  Logged out and logged in as me")))
-
 		})
 
 	})
