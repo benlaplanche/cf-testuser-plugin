4cb6beb3fbb5b29d609f225684d9886e36f78f51
Ben Laplanche <blaplanche@pivotal.io>
2015-02-13 17:32:56 +0000

    removed debug output

--- test_user_cmd_test.go
+++ test_user_cmd_test.go
@@ -1,7 +1,7 @@
 package main_test
 
 import (
-	"fmt"
+	"errors"
 	. "github.com/benlaplanche/cf-testuser-plugin"
 	"github.com/cloudfoundry/cli/plugin/fakes"
 	io_helpers "github.com/cloudfoundry/cli/testhelpers/io"
@@ -18,6 +18,7 @@ var _ = Describe("TestUserCmd", func() {
 		BeforeEach(func() {
 			fakeCliConnection = &fakes.FakeCliConnection{}
 			callCliCommandPlugin = &TestUser{}
+
 		})
 
 		It("returns an error if incorrect number of args supplied", func() {
@@ -34,7 +35,7 @@ var _ = Describe("TestUserCmd", func() {
 			output := io_helpers.CaptureOutput(func() {
 				callCliCommandPlugin.Run(fakeCliConnection, []string{"test-user", "me", "password"})
 			})
-			fmt.Println(output)
+
 			Expect(output[0]).To(Equal(colorstring.Color("[green][1/10]  Created user me")))
 
 			Expect(output[2]).To(Equal(colorstring.Color("[green][2/10]  Created Organisation development")))
