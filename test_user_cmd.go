651fb29c52a070a82392581a32ffac5636f00b9d
Ben Laplanche <ben@laplanche.co.uk>
2015-04-06 13:47:43 +0100

    split out plugin metadata and command info

--- test_user_cmd.go
+++ test_user_cmd.go
@@ -9,164 +9,6 @@ import (
 	"strings"
 )
 
-type CommandData struct {
-	Message            string
-	ExecutionArguments []string
-}
-
-type TestUser struct {
-	UserName    string
-	Password    string
-	OrgName     string
-	SpaceName   string
-	CmdRunCount int
-	Command     map[int]CommandData
-}
-
-func (c *TestUser) setCommands() {
-	c.Command = make(map[int]CommandData)
-
-	c.Command[1] = CommandData{
-		Message: "Created user " + c.UserName,
-		ExecutionArguments: []string{
-			"create-user",
-			c.UserName,
-			c.Password,
-		},
-	}
-
-	c.Command[2] = CommandData{
-		Message: "Created Organisation " + c.OrgName,
-		ExecutionArguments: []string{
-			"create-org",
-			c.OrgName,
-		},
-	}
-
-	c.Command[3] = CommandData{
-		Message: "Created Space " + c.SpaceName,
-		ExecutionArguments: []string{
-			"create-space",
-			c.SpaceName,
-		},
-	}
-
-	c.Command[4] = CommandData{
-		Message: "Assigned OrgManager to " + c.UserName + " in Org " + c.OrgName,
-		ExecutionArguments: []string{
-			"set-org-role",
-			c.UserName,
-			c.OrgName,
-			"OrgManager",
-		},
-	}
-
-	c.Command[5] = CommandData{
-		Message: "Assigned BillingManager to " + c.UserName + " in Org " + c.OrgName,
-		ExecutionArguments: []string{
-			"set-org-role",
-			c.UserName,
-			c.OrgName,
-			"BillingManager",
-		},
-	}
-
-	c.Command[6] = CommandData{
-		Message: "Assigned OrgAuditor to " + c.UserName + " in Org " + c.OrgName,
-		ExecutionArguments: []string{
-			"set-org-role",
-			c.UserName,
-			c.OrgName,
-			"AuditorManager",
-		},
-	}
-
-	c.Command[7] = CommandData{
-		Message: "Assigned SpaceManager to " + c.UserName + " in Space " + c.SpaceName,
-		ExecutionArguments: []string{
-			"set-space-role",
-			c.UserName,
-			c.OrgName,
-			c.SpaceName,
-			"SpaceManager",
-		},
-	}
-
-	c.Command[8] = CommandData{
-		Message: "Assigned SpaceDeveloper to " + c.UserName + " in Space " + c.SpaceName,
-		ExecutionArguments: []string{
-			"set-org-role",
-			c.UserName,
-			c.OrgName,
-			c.SpaceName,
-			"SpaceDeveloper",
-		},
-	}
-
-	c.Command[9] = CommandData{
-		Message: "Assigned SpaceAuditor to " + c.UserName + " in Space " + c.SpaceName,
-		ExecutionArguments: []string{
-			"set-org-role",
-			c.UserName,
-			c.OrgName,
-			c.SpaceName,
-			"SpaceAuditor",
-		},
-	}
-
-}
-
-const CmdTotalCount = 10
-const DefaultOrg = "development"
-const DefaultSpace = "development"
-
-func (t TestUser) commandCounter() (count string) {
-	current := strconv.Itoa(t.CmdRunCount)
-	total := strconv.Itoa(CmdTotalCount)
-
-	return "[" + current + "/" + total + "] "
-}
-
-func (c *TestUser) setProperties(args []string) {
-
-	c.UserName = args[1]
-	c.Password = args[2]
-	c.CmdRunCount = 1
-
-	if len(args) >= 4 {
-		c.OrgName = args[3]
-	} else {
-		c.OrgName = DefaultOrg
-	}
-
-	if len(args) >= 5 {
-		c.SpaceName = args[4]
-	} else {
-		c.SpaceName = DefaultSpace
-	}
-
-}
-
-func (c *TestUser) GetMetadata() plugin.PluginMetadata {
-	return plugin.PluginMetadata{
-		Name: "TestUser",
-		Version: plugin.VersionType{
-			Major: 0,
-			Minor: 1,
-			Build: 0,
-		},
-		Commands: []plugin.Command{
-			{
-				Name:     "test-user",
-				HelpText: "Create a user and assign all possible permissions, organisation and space are created if they do not already exist as well. If no organisation or space name are specified then the default value of 'development' is used",
-				UsageDetails: plugin.Usage{
-					Usage: "cf test-user <username> <password> <optional org name> <optional space name>",
-				},
-			},
-		},
-	}
-}
-
 func main() {
 	plugin.Start(new(TestUser))
 }
@@ -195,6 +37,26 @@ func (c *TestUser) Run(cliConnection plugin.CliConnection, args []string) {
 				return
 			}
 		}
+	}
+}
+
+func (c *TestUser) GetMetadata() plugin.PluginMetadata {
+	return plugin.PluginMetadata{
+		Name: "TestUser",
+		Version: plugin.VersionType{
+			Major: 0,
+			Minor: 1,
+			Build: 0,
+		},
+		Commands: []plugin.Command{
+			{
+				Name:     "test-user",
+				HelpText: "Create a user and assign all possible permissions, organisation and space are created if they do not already exist as well. If no organisation or space name are specified then the default value of 'development' is used",
+				UsageDetails: plugin.Usage{
+					Usage: "cf test-user <username> <password> <optional org name> <optional space name>",
+				},
+			},
+		},
 	}
 }
 
@@ -228,4 +90,31 @@ func (t *TestUser) printMessages(output []string, status []int) {
 
 		t.CmdRunCount++
 	}
+}
+
+func (t TestUser) commandCounter() (count string) {
+	current := strconv.Itoa(t.CmdRunCount)
+	total := strconv.Itoa(CmdTotalCount)
+
+	return "[" + current + "/" + total + "] "
+}
+
+func (c *TestUser) setProperties(args []string) {
+
+	c.UserName = args[1]
+	c.Password = args[2]
+	c.CmdRunCount = 1
+
+	if len(args) >= 4 {
+		c.OrgName = args[3]
+	} else {
+		c.OrgName = DefaultOrg
+	}
+
+	if len(args) >= 5 {
+		c.SpaceName = args[4]
+	} else {
+		c.SpaceName = DefaultSpace
+	}
+
 }
